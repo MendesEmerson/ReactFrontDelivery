389cb9df4813f7e91949898270a4344866b77da9
Emerson Mendes <devmendesc@gmail.com>
2023-05-29 01:25:38 -0300

    primeira finalização do component de itensCard

--- src/components/cardItem/index.tsx
+++ src/components/cardItem/index.tsx
@@ -1,13 +1,12 @@
-import { Flex, Heading, Stack, Text } from "@chakra-ui/layout";
-import { Card, CardBody } from "@chakra-ui/react";
-import { ImageProps, Image } from "@chakra-ui/react";
+import { Center, Flex, Heading, Stack, Text, Wrap } from "@chakra-ui/layout";
+import { Card, CardBody, Image, ImageProps } from "@chakra-ui/react";
 
 interface IItemCardProps {
     itemName: string;
     clientName: string;
     dataPedido: string;
     dataFinalização?: string;
-    image: ImageProps;
+    image: string
 }
 
 export function ItemCardComponent({
@@ -18,32 +17,38 @@ export function ItemCardComponent({
     image
 }: IItemCardProps) {
     return (
-        <Flex flexDirection={"column"} justifyContent={"center"}>
-            <Card maxW="sm">
-                <CardBody>
-                    <Image
-                        src={image.src}
-                        alt={image.alt}
-                        borderRadius={image.borderRadius}
-                    />
-                    <Stack mt='6' spacing='3'>
-                        <Heading size='md'>{itemName}</Heading>
-                        <Text>
-                            {clientName}
-                        </Text>
-                        <Text>
-                            {dataPedido}
-                        </Text>
-                        {dataFinalização ? (
-                            <Text >
-                                {dataFinalização}
-                            </Text>
-                        ) : (
-                            null
-                        )}
+        <Flex flexDirection="column" justifyContent="center" margin="25px" width={"90%"}>
+            <Card
+                padding={"10px"}
+                direction={{ base: "column", sm: "row" }}
+                overflow="hidden"
+                variant="outline"
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                height={"110px"}
+                width={"75%"}
+                bg={"blue.400"}
+                borderRadius={"24px"}
+                _hover={{ transform: "scale(1.05)", transition: "transform 0.2s" }}
+            >
+                <Flex alignItems={'Center'}>
+                    <Image borderRadius={"12px"} boxSize={"85px"} src={image} />
 
+
+                    <Stack>
+                        <CardBody>
+                            <Heading size="md">{itemName}</Heading>
+
+                            <Text >Cliente: {clientName}</Text>
+                            <Text >Data do pedido: {dataPedido}</Text>
+                            {dataFinalização && (
+                                <Text>{dataFinalização}</Text>
+                            )}
+                        </CardBody>
                     </Stack>
-                </CardBody>
+                </Flex>
+
             </Card>
         </Flex>
     );
--- src/pages/entregador/index.tsx
+++ src/pages/entregador/index.tsx
@@ -1,57 +1,65 @@
 import { Flex } from "@chakra-ui/layout";
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
 import { useState } from "react";
+import { ItemCardComponent } from "../../components/cardItem";
+import { Image } from "@chakra-ui/image";
 
 interface IItem {
-  itemName: string;
-  clientName: string;
-  dataPedido: string;
+    itemName: string;
+    clientName: string;
+    dataPedido: string;
+    image: string
 }
 
 export function EntregadorPage() {
-  const [entregasDisponiveis, setEntregasDisponiveis] = useState<IItem[]>([
-    {
-      itemName: "Pizza de Calabresa",
-      clientName: "Emerson",
-      dataPedido: "28/05/2023 23:25",
-    },
-    {
-      itemName: "Hamburguer",
-      clientName: "Juan",
-      dataPedido: "28/05/2023 23:05",
-    },
-  ]);
+    const [entregasDisponiveis, setEntregasDisponiveis] = useState<IItem[]>([
+        {
+            itemName: "Pizza de Calabresa",
+            clientName: "Emerson",
+            dataPedido: "28/05/2023 23:25",
+            image: "https://pastapizza.com.br/wp-content/uploads/2017/07/Pizza-Pizzaria-Forno-Forza-Express.jpg"
+        },
+        {
+            itemName: "Hamburguer",
+            clientName: "Juan",
+            dataPedido: "28/05/2023 23:05",
+            image: "https://supermercadosrondon.com.br/guiadecarnes/images/postagens/quer_fazer_hamburger_artesanal_perfeito_2019-05-14.jpg"
+        },
+    ]);
 
-  return (
-    <Flex
-      height={"100vh"}
-      width={"100%"}
-      justifyContent={"flex-start"}
-      alignItems={"center"}
-      flexDirection={"column"}
-    >
-      <Tabs variant="soft-rounded" colorScheme="green" margin={"20px"}>
-        <TabList>
-          <Tab>Entregas Disponiveis</Tab>
-          <Tab>Entregas em Andamento</Tab>
-          <Tab>Entregas Finalizadas</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            {entregasDisponiveis.map((entrega, index) => (
-              <p key={index}>
-                Item: {entrega.itemName} - Cliente: {entrega.clientName} - Data do Pedido: {entrega.dataPedido}
-              </p>
-            ))}
-          </TabPanel>
-          <TabPanel>
-            <p>Aqui vai entrar uma lista com todas as entregas em andamento!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>Aqui vai entrar uma lista com todas as entregas realizadas!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Flex>
-  );
+    return (
+        <Flex
+            height={"100vh"}
+            width={"100%"}
+            justifyContent={"flex-start"}
+            alignItems={"center"}
+            flexDirection={"column"}
+        >
+            <Tabs variant="soft-rounded" colorScheme="green" margin={"20px"}>
+                <TabList>
+                    <Tab>Entregas Disponiveis</Tab>
+                    <Tab>Entregas em Andamento</Tab>
+                    <Tab>Entregas Finalizadas</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        {entregasDisponiveis.map((entrega, index) => (
+                            <ItemCardComponent
+                                clientName={entrega.clientName}
+                                itemName={entrega.itemName}
+                                dataPedido={entrega.dataPedido}
+                                image={entrega.image}
+                            />
+                        ))}
+                    </TabPanel>
+                    <TabPanel>
+                        <p>Aqui vai entrar uma lista com todas as entregas em andamento!</p>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>Aqui vai entrar uma lista com todas as entregas realizadas!</p>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </Flex>
+    );
 }
